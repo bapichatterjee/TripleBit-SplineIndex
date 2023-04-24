dfb0fd34dc8bd674958cb84644886d2dcc0e3f34
nandikajain <jainnandika@gmail.com>
2023-04-24 06:41:30 +0530

    Added smaller dblp triple file

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ pushd ./bin/lrelease
 make -C ../../
 rm -rf test_build_dir/
 mkdir test_build_dir/
-./buildTripleBitFromN3 data.n3 test_build_dir/
+./buildTripleBitFromN3 tailored_data.n3 test_build_dir/
 ./triplebitQuery test_build_dir/ test_queries/
 popd
